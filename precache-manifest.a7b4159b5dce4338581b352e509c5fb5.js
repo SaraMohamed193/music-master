3f6c5ab2351f4f2425ae592ac3672fb0e7cfa835
Sara Borham <saraborham193@yahoo.com>
2019-04-20 20:56:26 +0200

    Updates

---
+++ precache-manifest.a7b4159b5dce4338581b352e509c5fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/music-master/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f18ccbcf8e95bf1e5d0",
+    "url": "/music-master/static/js/runtime~main.f9c15571.js"
+  },
+  {
+    "revision": "fb7f63e1f01ff1da86c6",
+    "url": "/music-master/static/js/main.31780fc8.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/music-master/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "fb7f63e1f01ff1da86c6",
+    "url": "/music-master/static/css/main.e664e451.chunk.css"
+  },
+  {
+    "revision": "dfd4c00c6cba82221794332877fd8a6b",
+    "url": "/music-master/index.html"
+  }
+];
